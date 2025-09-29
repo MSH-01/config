93050c105781a74dbf0a9759a418627d71f083b5
Mohammed H <mo@querio.ai>
2025-09-29 11:49:16 +0100

    fix: cleanup

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,39 +10,38 @@ vim.g.mapleader = " "
 -- bootstrap lazy.nvim if not installed
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", lazypath,
-  })
+	vim.fn.system({
+		"git", "clone", "--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  { "williamboman/mason.nvim" },
+	{ "williamboman/mason.nvim" },
 	-- Search my shit up
-	{ "nvim-telescope/telescope.nvim", tag = "0.1.8" },
-	{ "nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"},
-	{ "
+	{ "nvim-telescope/telescope.nvim",   tag = "0.1.8" },
+	{ "nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate" },
 	-- Comment out some shit ez
 	{ "tpope/vim-commentary" },
-  -- Language server
+	-- Language server
 	{ "neovim/nvim-lspconfig" },
-  { "nvim-lua/plenary.nvim" },
-  { "MunifTanjim/nui.nvim" },
-  { "nvim-tree/nvim-web-devicons" },
-	-- Get rid of any tree for now..
-  -- { "nvim-neo-tree/neo-tree.nvim" },
+	{ "nvim-lua/plenary.nvim" },
+	{ "MunifTanjim/nui.nvim" },
+	{ "nvim-tree/nvim-web-devicons" },
+	-- Get rid of neotree for now..
+	-- { "nvim-neo-tree/neo-tree.nvim" },
 	{
-    "i3d/vim-jimbothemes",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.opt.background = "dark"
-      vim.opt.termguicolors = true
-      vim.cmd.colorscheme("batman")
-    end,
-  },
+		"i3d/vim-jimbothemes",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.opt.background = "dark"
+			vim.opt.termguicolors = true
+			vim.cmd.colorscheme("batman")
+		end,
+	},
 })
 
 require "mason".setup()
@@ -91,17 +90,17 @@ require "mason".setup()
 local builtin = require('telescope.builtin')
 
 -- Keymaps
+-- LSP
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+vim.keymap.set('n', 'K', vim.lsp.buf.hover)
+
+-- Reeeeload it
 vim.keymap.set('n', '<leader>o', ':Lazy sync<CR>')
 -- Neo-tree keymap
 -- vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>')
 -- Telescope keymap
-vim.keymap.set('n', '<leader>ff', function()
-	builtin.find_files({
-		hidden = false,
-		no_ignore = false
-	})
-end, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
